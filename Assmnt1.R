e0deab5c2b53c0fc6b6bad479db77b8f210b77a6
konstantin-klein <konstantin.klein@ensoltech.ru>
2015-02-07 12:22:06 +0300

    First commit

--- Assmnt1.R
+++ Assmnt1.R
@@ -1,51 +1,38 @@
----
-title: "Reproducible Research: Peer Assessment 1"
-output: 
-  html_document:
-    keep_md: true
----
 
-## Preparing the environment
-
-```{r, echo = TRUE, results = 'hide', message=FALSE}
 library(dplyr)
 Sys.setlocale("LC_TIME", "en_US.UTF-8")
-```
 
 ## Loading and preprocessing the data
 
-```{r}
+
 if(!file.exists("activity.zip")) {
+      print(noquote("downloading datafile from URL"))
       fileUrl <-"https://d396qusza40orc.cloudfront.net/repdata%2Fdata%2Factivity.zip"
       download.file(fileUrl, "activity.zip", method = "curl")
-} 
+} else print(noquote("Datafile found"))
+
 filename = "activity.csv"
 
-if(!file.exists(filename)) unzip("activity.zip")
+if(!file.exists(filename)){
+      print(noquote("No dataset found. Unzipping data from archive"))
+      unzip("activity.zip")}
 
-activity <- read.csv(filename, header = TRUE, sep = ',', quote = '\"',stringsAsFactors = FALSE)
+print(noquote("Reading data into R"))
+activity <- read.csv("activity.csv", header = TRUE, sep = ',', quote = '\"',stringsAsFactors = FALSE)
+
 activity$date <- as.Date(activity$date, "%Y-%m-%d")
+
 activity <- tbl_df(activity)
-```
 
-## What is mean total number of steps taken per day?
-
-```{r}
+# summarising data
+print(noquote("Calculating annual emissions"))
 totalByDate <- activity %>% group_by(date) %>% summarize(total_steps=sum(steps))
 
+print(noquote("Building a plot"))
 with(totalByDate, plot(date, total_steps, main = 'Total steps per day', ylab = 'Steps', xlab = 'Date'))
 mean_steps <- mean(totalByDate$total_steps,na.rm = TRUE)
 median_steps <- median(totalByDate$total_steps,na.rm = TRUE)      
 abline(h=mean_steps, col = 'green')
 abline(h=median_steps, col = 'blue')
-```
-
-## What is the average daily activity pattern?
 
 
-
-## Imputing missing values
-
-
-
-## Are there differences in activity patterns between weekdays and weekends?
